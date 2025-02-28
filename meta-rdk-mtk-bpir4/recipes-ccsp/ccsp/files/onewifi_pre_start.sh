82ed268d4fca90d55eca04f8256195aab5f59bf5
pratheepa1995 <pratheepa_p@comcast.com>
2025-02-28 16:28:35 +0530

    RDKBACCL-388 : WPS support in Banana Pi R4
    Reason for change: Added required script for WPS Push button functionality
    
    Test Procedure: Able to connect clients when WPS Push Button is pressed in BPI. Screenshot attached in JIRA
    Risks: Low
    
    Signed-off-by: pratheepa1995 <pratheepa_p@comcast.com>

--- meta-rdk-mtk-bpir4/recipes-ccsp/ccsp/files/onewifi_pre_start.sh
+++ meta-rdk-mtk-bpir4/recipes-ccsp/ccsp/files/onewifi_pre_start.sh
@@ -29,4 +29,33 @@ ifconfig wifi0 up
 ifconfig wifi1 up
 #ifconfig wifi2 up
 
+modprobe gpio_keys
+EVENT_DEVICE="/dev/input/event0"  # Confirmed from evtest
+VAP_INDEX_2G=0  # Virtual AP index for OneWifi
+VAP_INDEX_5G=1  # Virtual AP index for OneWifi
+VAP_INDEX_6G=2  # Virtual AP index for OneWifi
+LOGFILE="/tmp/wps_trigger.log"
+
+echo "Listening for WPS button press on $EVENT_DEVICE..." | tee -a $LOGFILE
+
+# Read event stream and trigger WPS when KEY_WPS_BUTTON (529) is detected
+evtest "$EVENT_DEVICE" | while read line; do
+    if echo "$line" | grep -q "code 529 (KEY_WPS_BUTTON), value 1"; then
+        echo "✅ WPS Button Pressed! Triggering OneWifi WPS for 2G, 5G and 6G..." | tee -a $LOGFILE
+
+        # Kill any existing interactive session before triggering WPS
+        pkill -f onewifi_component_test_app
+
+        # Run WPS command and log output
+        echo "Executing: echo 'wps $VAP_INDEX' | /usr/bin/onewifi_component_test_app" | tee -a $LOGFILE
+        echo "wps $VAP_INDEX_2G" | /usr/bin/onewifi_component_test_app >> $LOGFILE 2>&1
+
+        echo "wps $VAP_INDEX_5G" | /usr/bin/onewifi_component_test_app >> $LOGFILE 2>&1
+
+        echo "wps $VAP_INDEX_6G" | /usr/bin/onewifi_component_test_app >> $LOGFILE 2>&1
+
+        sleep 5  # Prevent multiple triggers within 5 seconds
+    fi
+done
+
 exit 0
