6f0867314df00c97beecf46c855821befe41b07b
Dharahasini Gangalapudi <54323888+hasinireddy23@users.noreply.github.com>
2026-07-30 03:00:52 -0400

    Create test_snippet.c

---
+++ test_snippet.c
@@ -0,0 +1,40 @@
+/* adler32.c -- compute the Adler-32 checksum of a data stream
+ * Copyright (C) 1995-2011 Mark Adler
+ * For conditions of distribution and use, see copyright notice in zlib.h
+ */
+
+#define BASE 65521U     /* largest prime smaller than 65536 */
+#define NMAX 5552       /* NMAX is the largest n such that 255n(n+1)/2 + (n+1)(BASE-1) <= 2^32-1 */
+
+#define DO1(buf,i)  {adler += (buf)[i]; sum2 += adler;}
+#define DO2(buf,i)  DO1(buf,i); DO1(buf,i+1);
+#define DO4(buf,i)  DO2(buf,i); DO2(buf,i+2);
+#define DO8(buf,i)  DO4(buf,i); DO4(buf,i+4);
+#define DO16(buf)   DO8(buf,0); DO8(buf,8);
+
+unsigned long adler32(unsigned long adler, const unsigned char *buf, unsigned int len) {
+    unsigned long sum2;
+    unsigned int n;
+
+    sum2 = (adler >> 16) & 0xffff;
+    adler &= 0xffff;
+
+    if (buf == ((void *)0)) return 1L;
+
+    while (len > 0) {
+        n = len < NMAX ? len : NMAX;
+        len -= n;
+        while (n >= 16) {
+            DO16(buf);
+            buf += 16;
+            n -= 16;
+        }
+        while (n > 0) {
+            sum2 += (adler += *buf++);
+            n--;
+        }
+        adler %= BASE;
+        sum2 %= BASE;
+    }
+    return adler | (sum2 << 16);
+}
